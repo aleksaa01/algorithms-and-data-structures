7e7775a029a7e70895e966f8a50771b0fba61c46
Aleksa <some12curious@gmail.com>
2018-07-05 13:27:14 +0200

    corrected the name

---
+++ data-structures/Trees/BinaryTree/binary_search_tree.py
@@ -0,0 +1,10 @@
+class Node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
+
+
+class BinarySearchTree:
+    pass
