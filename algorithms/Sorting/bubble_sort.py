cad4f5a0a58c8ada627d11ea9eef58035d026582
aleksaa01 <some12curious@gmail.com>
2019-01-28 13:53:33 +0100

    Added new implementation of bubble sort with optimization for already sorted arrays. Added few more tests.

--- algorithms/Sorting/bubble_sort.py
+++ algorithms/Sorting/bubble_sort.py
@@ -4,20 +4,67 @@ def bubble_sort(array):
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
 
+def bubble_sort_improved(array):
+    x = 0
+    n = len(array)
+    while x < n - 1:
+        swaped = False
+        for j in range(n - x - 1):
+            if array[j] > array[j + 1]:
+                swaped = True
+                array[j], array[j + 1] = array[j + 1], array[j]
+        if not swaped:
+            break
+        x += 1
+
 
 if __name__ == '__main__':
     import time
+    import random
 
     def test1():
-        import random
         l1 = [i for i in range(5000)]
         l2 = l1.copy()
         random.shuffle(l2)
-        print('Sorting 5000 items...')
+        print('(bubble sort) Sorting 5000 unsorted items...')
         t1 = time.time()
         bubble_sort(l2)
         t2 = time.time()
         print('Time took:', t2 - t1)
         assert l1 == l2
 
+    def test2():
+        l1 = [i for i in range(5000)]
+        l2 = l1.copy()
+        random.shuffle(l2)
+        print('(Improved bubble sort) Sorting 5000 unsorted items...')
+        t1 = time.time()
+        bubble_sort_improved(l2)
+        t2 = time.time()
+        print('Time took:', t2 - t1)
+        assert l1 == l2
+
+    def test3():
+        l1 = [i for i in range(5000)]
+        l2 = l1.copy()
+        print('(bubble sort) Sorting 5000 sorted items...')
+        t1 = time.time()
+        bubble_sort(l2)
+        t2 = time.time()
+        print('Time took:', t2 - t1)
+        assert l1 == l2
+
+    def test4():
+        l1 = [i for i in range(5000)]
+        l2 = l1.copy()
+        print('(Improved bubble sort) Sorting 5000 sorted items...')
+        t1 = time.time()
+        bubble_sort_improved(l2)
+        t2 = time.time()
+        print('Time took:', t2 - t1)
+        assert l1 == l2
+
     test1()
+    test2()
+    test3()
+    test4()
